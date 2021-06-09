565b9b49da6d3d024c7318a864b2b16211ead04a
Thore Strassburg <thore@weilbier.net>
2021-06-09 11:10:13 +0200

    Use new vim.opt feature to set options in Lua

--- init.lua
+++ init.lua
@@ -7,53 +7,46 @@ vim.cmd("filetype plugin on")
 vim.cmd("filetype indent on")
 vim.cmd("syntax on")
 
-vim.o.encoding = 'utf-8'
-vim.wo.spell = false -- TODO: treesitter is annoying atm.
+vim.opt.encoding = 'utf-8'
+vim.opt.spell = false -- TODO: treesitter is annoying atm.
 -- vim.o.spellfile = vim.fn.stdpath('data') .. '/spell/en.utf-8.add'
-vim.wo.cursorline = true
-vim.o.termguicolors = true
-vim.wo.number = true
-vim.wo.conceallevel = 2
-vim.wo.concealcursor = 'c'
-vim.bo.tabstop = 2
-vim.bo.softtabstop = 2
-vim.bo.shiftwidth = 2
-vim.bo.expandtab = true
-vim.bo.smartindent = true
-vim.o.shiftround = true
-vim.bo.formatoptions = 'j1cql'
-vim.bo.textwidth = 80
-vim.wo.linebreak = true
-vim.o.whichwrap = 'b,s,h,l'
-vim.o.showmode = false
-vim.o.completeopt = 'menuone,noinsert,noselect'
-vim.o.shortmess = vim.o.shortmess .. 'c'
-vim.o.showmatch = true
-vim.o.scrolloff = 2
-vim.o.ignorecase = true
-vim.o.smartcase = true
+vim.opt.cursorline = true
+vim.opt.termguicolors = true
+vim.opt.number = true
+vim.opt.conceallevel = 2
+vim.opt.concealcursor = 'c'
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.shiftround = true
+vim.opt.formatoptions = 'j1cql'
+vim.opt.textwidth = 80
+vim.opt.linebreak = true
+vim.opt.whichwrap = 'b,s,h,l'
+vim.opt.showmode = false
+vim.opt.completeopt = 'menuone,noinsert,noselect'
+vim.opt.shortmess = vim.o.shortmess .. 'c'
+vim.opt.showmatch = true
+vim.opt.scrolloff = 2
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 -- vim.o.viminfo = '100,n' .. vim.fn.stdpath('data') .. '/files/info/viminfo'
-vim.bo.swapfile = false
-vim.bo.undofile = true
-vim.o.updatetime = 1000
-vim.o.updatecount = 0
-vim.wo.foldlevel = 1
-vim.wo.foldcolumn = 'auto:9'
-vim.o.hidden = true
+vim.opt.swapfile = false
+vim.opt.undofile = true
+vim.opt.updatetime = 1000
+vim.opt.updatecount = 0
+vim.opt.foldlevel = 1
+vim.opt.foldcolumn = 'auto:5'
+vim.opt.hidden = true
 -- vim.o.lazyredraw = true
-vim.o.pumheight = 30
+vim.opt.pumheight = 30
 -- vim.o.showtabline = 2
-vim.o.joinspaces = false
-vim.o.inccommand = 'split'
-vim.o.fillchars = 'vert:█,diff:█,fold:⎯,foldclose:,foldopen:,foldsep:│'
-vim.o.listchars = 'eol:﬋,tab:ﲒ,space:.,trail:.,extends:ﰣ,precedes:﬌,nbsp:.'
-vim.o.diffopt = 'internal,filler,closeoff,vertical,context:3,algorithm:patience'
-vim.o.viewoptions = 'cursor,folds,slash,unix'
-vim.g.netrw_home = vim.fn.stdpath('data') .. '/netrwhist'
-
--- TODO: Remove when PR with `vim.opt` got merged.
-vim.api.nvim_set_option("tabstop", 2)
-vim.api.nvim_set_option("softtabstop", 2)
-vim.api.nvim_set_option("shiftwidth", 2)
-vim.api.nvim_set_option("expandtab", true)
-vim.api.nvim_set_option("shiftround", true)
+vim.opt.joinspaces = false
+vim.opt.inccommand = 'split'
+vim.opt.fillchars = 'vert:█,diff:█,fold:⎯,foldclose:,foldopen:,foldsep:│'
+vim.opt.listchars = 'eol:﬋,tab:ﲒ,space:.,trail:.,extends:ﰣ,precedes:﬌,nbsp:.'
+vim.opt.diffopt = 'internal,filler,closeoff,vertical,context:3,algorithm:patience'
+vim.opt.viewoptions = 'cursor,folds,slash,unix'
+-- vim.opt.netrw_home = vim.fn.stdpath('data') .. '/netrwhist'
