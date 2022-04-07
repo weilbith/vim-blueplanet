2f1dc32accd439e620335df998d70689243e0089
Thore Strassburg <thore@weilbier.net>
2022-04-07 14:39:59 +0200

    use strict nvim_command for plugin manager

--- pack/plugins/opt/plugin_manager/lua/plugin_manager.lua
+++ pack/plugins/opt/plugin_manager/lua/plugin_manager.lua
@@ -3,17 +3,18 @@ local packer_source_url = 'https://github.com/wbthomason/packer.nvim'
 local function install_packer(package_path)
   local installation_path = package_path .. '/pack/packer/opt/packer.nvim'
 
-  if vim.fn.empty(vim.fn.glob(installation_path)) > 0 then
-    vim.cmd(string.format('!git clone %s %s', packer_source_url, installation_path))
+  if #vim.api.nvim_call_function('glob', { installation_path }) == 0 then
+    vim.api.nvim_command(string.format('!git clone %s %s', packer_source_url, installation_path))
   end
 end
 
 local function setup(package_path)
   install_packer(package_path)
   vim.o.packpath = vim.o.packpath .. ',' .. package_path
-  vim.cmd('packadd packer.nvim')
+  vim.api.nvim_command('packadd packer.nvim')
   require('packer').init({
     package_root = package_path .. '/pack',
+    autoremove = true,
     display = {
       open_fn = require('packer.util').float,
       open_cmd = require('packer.util').float,
@@ -24,7 +25,7 @@ local function setup(package_path)
 end
 
 local function update()
-  vim.cmd('runtime! OPT register_plugin.lua')
+  vim.api.nvim_command('runtime! OPT register_plugin.lua')
   require('packer').sync()
 end
 
