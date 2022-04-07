f4b72ef771fa5eb31840ae23fdaf5ce874ea3e3e
Thore Strassburg <thore@weilbier.net>
2022-04-07 14:38:33 +0200

    LuaSnip: use virtual text now with removal fix

--- pack/plugins/opt/luasnip/plugin/setup.lua
+++ pack/plugins/opt/luasnip/plugin/setup.lua
@@ -1,10 +1,30 @@
 local luasnip = require('luasnip')
+local snippet_types = require('luasnip.util.types')
 local SnippetLibrary = require('snippets.SnippetLibrary')
 
 require('luasnip.config').setup({
   enable_autosnippets = true,
   updateevents = 'TextChanged,TextChangedI',
+  delete_check_events = 'TextChanged',
   ft_func = require('luasnip.extras.filetype_functions').from_pos_or_filetype,
+  ext_opts = {
+    [snippet_types.insertNode] = {
+      active = {
+        virt_text = { { '● insert', 'GreenBold' } },
+      },
+      passive = {
+        virt_text = { { '● insert', 'BlueBold' } },
+      },
+    },
+    [snippet_types.choiceNode] = {
+      active = {
+        virt_text = { { '● choice', 'GreenBold' } },
+      },
+      passive = {
+        virt_text = { { '● choice', 'PurpleBold' } },
+      },
+    },
+  },
 })
 
 luasnip.snippets = SnippetLibrary:new('snippets')
