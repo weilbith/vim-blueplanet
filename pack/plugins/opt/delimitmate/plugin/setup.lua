4cb97d79dd4e50c9062c32cb36acb4cc09ef01f8
Thore Strassburg <thore@weilbier.net>
2022-04-25 15:33:38 +0200

    DelimitMate: make reload command silent

--- pack/plugins/opt/delimitmate/plugin/setup.lua
+++ pack/plugins/opt/delimitmate/plugin/setup.lua
@@ -3,4 +3,4 @@ vim.g.delimitMate_expand_cr = 2
 vim.g.delimitMate_expand_space = 1
 vim.g.delimitMate_balance_matchpairs = 1
 
-vim.api.nvim_command('DelimitMateReload')
+vim.api.nvim_command('silent DelimitMateReload')
