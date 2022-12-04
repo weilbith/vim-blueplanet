51175f16622952ab803714c8eb3d53edce1ab65f
Thore Strassburg <thore@weilbier.net>
2022-12-04 14:01:26 +0100

    Ufo: use treesitter for YAML files

--- pack/plugins/opt/ufo/plugin/setup.lua
+++ pack/plugins/opt/ufo/plugin/setup.lua
@@ -1,6 +1,10 @@
+local filetype_provider_mapping = {
+  yaml = { 'treesitter', 'indent' },
+}
+
 require('ufo').setup({
-  provider_selector = function()
-    return { 'lsp', 'indent' }
+  provider_selector = function(_, filetype)
+    return filetype_provider_mapping[filetype] or { 'lsp', 'indent' }
   end,
   fold_virt_text_handler = function(fold_text, start_line_number, end_line_number)
     local line_count = end_line_number - start_line_number
