a77147e1b28e6e6adf89fa0114fa09baa09ab433
Thore Strassburg <thore@weilbier.net>
2022-12-11 00:03:48 +0100

    Matchup: enable Treesitter support

--- pack/plugins/opt/matchup/plugin/setup.lua
+++ pack/plugins/opt/matchup/plugin/setup.lua
@@ -3,3 +3,9 @@ vim.g.matchup_matchparen_status_offscreen = 'popup'
 vim.g.matchup_matchparen_defer = 1
 vim.g.matchup_motion_cursor_end = 1
 vim.g.matchup_matchparen_hi_surround_always = 1
+
+require('nvim-treesitter.configs').setup({
+  matchup = {
+    enable = true,
+  },
+})
--- pack/plugins/opt/matchup/register_plugin.lua
+++ pack/plugins/opt/matchup/register_plugin.lua
@@ -1,5 +1,6 @@
 require('packer').use({
   'andymass/vim-matchup',
+  requires = 'nvim-treesitter/nvim-treesitter',
   event = 'VimEnter',
   config = 'vim.cmd("packadd matchup")',
 })
