46de5ae9f3d23d6e3f1b760b9622b52a2f2c598b
Thore Strassburg <thore@weilbier.net>
2022-12-11 13:09:51 +0100

    Heirline: simplify configuration

--- pack/plugins/opt/heirline/plugin/setup.lua
+++ pack/plugins/opt/heirline/plugin/setup.lua
@@ -1,80 +1,13 @@
-local buffer_matches = require('heirline.conditions').buffer_matches
-local pick_child_on_condition = require('heirline.utils').pick_child_on_condition
-
 local Breadcrumbs = require('heirline_custom.components.breadcumbs')
 local Tests = require('heirline_custom.components.tests')
 local Project = require('heirline_custom.components.project')
 local Git = require('heirline_custom.components.git')
 local Lsp = require('heirline_custom.components.lsp')
 
-local WinBars = {
-  init = pick_child_on_condition,
-  {
-    condition = function()
-      local window_configuration = vim.api.nvim_win_get_config(0)
-      local is_floating_window = window_configuration.relative ~= ''
+local Space = { provider = ' ' }
+local Gap = { provider = '%=' }
 
-      if is_floating_window then
-        return false
-      else
-        return buffer_matches({
-          buftype = { 'nofile', 'prompt', 'help', 'quickfix' },
-          filetype = {
-            '^git.*',
-            'fugitive',
-            'floaterm',
-            'TelescopePrompt',
-            'neo-tree',
-            'neo-tree-popup',
-          },
-        })
-      end
-    end,
-    provider = '',
-  },
-  {
-    { provider = ' ' },
-    Breadcrumbs,
-    { provider = '%=' },
-    Tests,
-    { provider = ' ' },
-  },
-}
+local WindowBar = { Space, Breadcrumbs, Gap, Tests, Space }
+local StatusBar = { Space, Project, Space, Git, Gap, Lsp, Space }
 
-local StatusBar = {
-  init = pick_child_on_condition,
-  {
-    condition = function()
-      local window_configuration = vim.api.nvim_win_get_config(0)
-      local is_floating_window = window_configuration.relative ~= ''
-
-      if is_floating_window then
-        return false
-      else
-        return buffer_matches({
-          buftype = { 'nofile', 'prompt', 'help', 'quickfix' },
-          filetype = {
-            '^git.*',
-            'fugitive',
-            'floaterm',
-            'TelescopePrompt',
-            'neo-tree',
-            'neo-tree-popup',
-          },
-        })
-      end
-    end,
-    provider = '',
-  },
-  {
-    { provider = ' ' },
-    Project,
-    { provider = ' ' },
-    Git,
-    { provider = '%=' },
-    Lsp,
-    { provider = ' ' },
-  },
-}
-
-require('heirline').setup(StatusBar, WinBars)
+require('heirline').setup(StatusBar, WindowBar)
