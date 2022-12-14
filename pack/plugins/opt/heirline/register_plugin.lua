c84bca4c670eacec96c6b92144d68e6dfee2c01b
Thore Strassburg <thore@weilbier.net>
2022-12-14 16:19:41 +0100

    Heirline: fix loading issue with colors
    When Packer was re-compiling the loading script, the order of the
    Heirline plugin and the one to set the colorscheme changed randomly. The
    problem is that Heirline tries to access some custom highlight groups.
    If they don't exist, the whole NeoVim startup kinda "fails".
    The "workaround" was to just re-compile it again and again until the
    order was correct again.
    
    To solve this more properly now, Heirline gets loaded lazily now. The
    issues was just that the `ColorScheme` event wasn't working as it is
    somehow too early in the event chain and the Packer script isn't sourced
    yet.
    For now, the `VimEnter` event seems to work reliably because here all
    `plugin/*` scripts are already sourced.

--- pack/plugins/opt/heirline/register_plugin.lua
+++ pack/plugins/opt/heirline/register_plugin.lua
@@ -1,5 +1,6 @@
 require('packer').use({
   'rebelot/heirline.nvim',
+  event = 'VimEnter',
   requires = {
     'kyazdani42/nvim-web-devicons',
     { 'SmiteshP/nvim-gps', requires = 'nvim-treesitter/nvim-treesitter' },
