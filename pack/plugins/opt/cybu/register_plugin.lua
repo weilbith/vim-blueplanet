204cc2a167e3e8394e9bbbc6f78af773a922532f
Thore Strassburg <thore@weilbier.net>
2022-12-14 16:23:41 +0100

    Cybu: remove plugin as not useful anymore

--- pack/plugins/opt/cybu/register_plugin.lua
+++
@@ -1,6 +0,0 @@
-require('packer').use({
-  'ghillb/cybu.nvim',
-  requires = { 'kyazdani42/nvim-web-devicons' },
-  keys = { '<Plug>(CybuNext)', '<Plug>(CybuPrev)' },
-  config = 'vim.cmd("packadd cybu")',
-})
