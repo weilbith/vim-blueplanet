ffc6c401f4f556c01712849d02d694f4839a0ff9
Thore Strassburg <thore@weilbier.net>
2023-06-22 15:52:52 +0200

    Other: add more mappings

--- pack/plugins/opt/other/plugin/setup.lua
+++ pack/plugins/opt/other/plugin/setup.lua
@@ -1,18 +1,28 @@
 require('other-nvim').setup({
   mappings = {
     {
-      pattern = 'src/(.*)/(.*).ts',
-      target = 'test/%1/%2.spec.ts',
+      pattern = 'src/.*/(.*).ts',
+      target = 'test/**/%1.spec.ts',
       context = 'tests',
     },
     {
-      pattern = 'test/(.*)/(.*).spec.ts',
-      target = 'src/%1/%2.ts',
+      pattern = 'test/.*/(.*).spec.ts',
+      target = 'src/**/%1.ts',
       context = 'source',
     },
     {
-      pattern = 'src/main/(.*)/(.*).spec.ts',
-      target = 'src/%1/%2.ts',
+      pattern = 'src/.*/(.*).vue',
+      target = 'test/**/%1.spec.ts',
+      context = 'tests',
+    },
+    {
+      pattern = 'test/.*/(.*).spec.ts',
+      target = 'src/**/%1.vue',
+      context = 'source',
+    },
+    {
+      pattern = 'src/main/.*/(.*).kt',
+      target = 'src/%1/%2Test.kt',
       context = 'source',
     },
   },
