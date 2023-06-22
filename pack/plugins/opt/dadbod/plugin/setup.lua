763dd6eeddd8d000d497962e8b9c52562f12ea4b
Thore Strassburg <thore@weilbier.net>
2023-06-22 15:13:09 +0200

    Dadbod: customize UI icons

--- pack/plugins/opt/dadbod/plugin/setup.lua
+++ pack/plugins/opt/dadbod/plugin/setup.lua
@@ -1,1 +1,29 @@
 vim.g.db_ui_auto_execute_table_helpers = 1
+vim.g.db_ui_use_nerd_fonts = true
+vim.g.db_ui_icons = {
+  expanded = {
+    db = '',
+    buffers = '+',
+    saved_queries = '+',
+    schemas = '+',
+    schema = '+',
+    tables = '+',
+    table = '+',
+  },
+  collapsed = {
+    db = '',
+    buffers = '-',
+    saved_queries = '-',
+    schemas = '-',
+    schema = '-',
+    tables = '-',
+    table = '-',
+  },
+  saved_query = '*',
+  new_query = '+',
+  tables = '',
+  buffers = '»',
+  add_connection = '+',
+  connection_ok = '',
+  connection_error = '',
+}
