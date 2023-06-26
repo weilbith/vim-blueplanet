2af0b9586e5a344a1ed079e1e3bd1bb15ee951f9
Thore Strassburg <thore@weilbier.net>
2023-06-26 15:53:08 +0200

    Neodim: remove in favor of native support
    Though the native one only works for language servers that send semantic
    tokens. It also doesn't hide related diagnostics.

--- pack/plugins/opt/neodim/plugin/setup.lua
+++
@@ -1,8 +0,0 @@
-require('neodim').setup({
-  alpha = 0.5,
-  blend_color = '#18191B',
-  hide = {
-    virtual_text = true,
-    signs = false,
-  },
-})
--- pack/plugins/opt/neodim/register_plugin.lua
+++
@@ -1,5 +0,0 @@
-require('packer').use({
-  'zbirenbaum/neodim',
-  event = 'LspAttach',
-  config = 'vim.cmd("packadd neodim")',
-})
