3c6b508715a7e4acd467aef14ce7bec11fe552af
Thore Strassburg <thore@weilbier.net>
2023-09-07 12:12:26 +0200

    Glance: register Vue SFC specific method

--- pack/plugins/opt/glance/plugin/setup.lua
+++ pack/plugins/opt/glance/plugin/setup.lua
@@ -1,5 +1,11 @@
 local glance = require('glance')
 
+glance.register_method({
+  name = 'vue_file_references',
+  method = 'volar/client/findFileReference',
+  label = 'Component Usages',
+})
+
 glance.setup({
   folds = {
     fold_closed = '',
---
+++ pack/plugins/start/glance/plugin/after/ftplugin/vue.vim
@@ -0,0 +1,1 @@
+nnoremap <buffer> <leader>fr <cmd>Glance vue_file_references<CR>
