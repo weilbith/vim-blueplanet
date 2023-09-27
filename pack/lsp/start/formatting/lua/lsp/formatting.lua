bc1484f16129701254f97a068fa45a33771a08c2
Thore Strassburg <thore@weilbier.net>
2023-09-27 17:10:39 +0200

    Use asynchronous LSP formatting

--- pack/lsp/start/formatting/lua/lsp/formatting.lua
+++ pack/lsp/start/formatting/lua/lsp/formatting.lua
@@ -12,39 +12,19 @@ local function formattingIsDisabledByUser()
   return vim.b.formatting_disabled or vim.g.formatting_disabled or false
 end
 
-local function handle_format_response(error, result, context)
-  if result == nil then
-    return
-  end
-
-  if error ~= nil then
-    vim.api.nvim_notify('Formatting has failed!', 4, {})
-    return
-  end
-
-  local buffer_number = context.bufnr
-  local buffer_modified = vim.api.nvim_buf_get_option(buffer_number, 'modified')
-
-  if not buffer_modified then
-    local preserved_view = vim.fn.winsaveview()
-    vim.lsp.util.apply_text_edits(result, buffer_number)
-    vim.fn.winrestview(preserved_view)
-  end
-end
-
-local function setup()
-  vim.lsp.handlers['textDocument/formatting'] = handle_format_response
-end
-
 local function formatBufferIfPossible()
   if doesAnyClientSupportFormatting() then
     if not formattingIsDisabledByUser() then
-      vim.lsp.buf.format()
+      vim.lsp.buf.format({
+        async = true,
+        filter = function(client)
+          return client.name ~= 'kotlin_language_server'
+        end,
+      })
     end
   end
 end
 
 return {
-  setup = setup,
   formatBufferIfPossible = formatBufferIfPossible,
 }
