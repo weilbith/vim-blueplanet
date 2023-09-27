a8f0f4165b72d3a3add97c405c7ada2ccdeabb53
Thore Strassburg <thore@weilbier.net>
2023-09-27 17:24:14 +0200

    LuaSnip: improve configuration

--- pack/plugins/opt/luasnip/plugin/setup.lua
+++ pack/plugins/opt/luasnip/plugin/setup.lua
@@ -1,32 +1,19 @@
 local snippet_types = require('luasnip.util.types')
+local extend_load_ft = require('luasnip.extras.filetype_functions').extend_load_ft
 
-require('luasnip.config').setup({
+require('luasnip').setup({
+  history = true,
   enable_autosnippets = true,
-  updateevents = 'TextChanged,TextChangedI',
   delete_check_events = 'TextChanged,InsertEnter',
+  -- Does not work?
+  load_ft_func = extend_load_ft({
+    typescript = { 'javascript' },
+    vue = { 'typescript', 'javascript' },
+  }),
   ext_opts = {
-    [snippet_types.insertNode] = {
-      active = {
-        virt_text = { { '', 'GreenBold' } },
-      },
-      passive = {
-        -- hl_group = 'GreenUnderline',
-        virt_text = { { '', 'GreenBold' } },
-      },
-      visited = {
-        virt_text = { { '', 'GreyBold' } },
-      },
-    },
     [snippet_types.choiceNode] = {
       active = {
-        virt_text = { { '', 'PurpleBold' } },
-      },
-      passive = {
-        -- hl_group = 'PurpleUnderline',
-        virt_text = { { '', 'PurpleBold' } },
-      },
-      visited = {
-        virt_text = { { '', 'GreyBold' } },
+        virt_text = { { '', 'PurpleBold' } },
       },
     },
   },
