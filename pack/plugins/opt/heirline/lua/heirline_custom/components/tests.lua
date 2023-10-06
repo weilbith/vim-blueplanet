03bb9d43618ae45e711acf0e3384cbbc4b1e2165
Thore Strassburg <thore@weilbier.net>
2023-10-06 13:59:25 +0200

    Heirline: adapt test block to use Neotest

--- pack/plugins/opt/heirline/lua/heirline_custom/components/tests.lua
+++ pack/plugins/opt/heirline/lua/heirline_custom/components/tests.lua
@@ -1,23 +1,59 @@
 local surround = require('heirline.utils').surround
 local get_highlight = require('heirline.utils').get_highlight
 
+local running_icon = (vim.api.nvim_call_function('sign_getdefined', { 'neotest_running' })[1] or {}).text
+  or ''
+local passed_icon = (vim.api.nvim_call_function('sign_getdefined', { 'neotest_passed' })[1] or {}).text
+  or ''
+local failed_icon = (vim.api.nvim_call_function('sign_getdefined', { 'neotest_failed' })[1] or {}).text
+  or ''
+
+local function has_test_cases(buffer_number)
+  for _, adapter_identifier in pairs(require('neotest').state.adapter_ids()) do
+    local positions =
+      require('neotest').state.positions(adapter_identifier, { buffer = buffer_number })
+    if positions ~= nil then
+      return true
+    end
+  end
+
+  return false
+end
+
+local function get_test_case_numbers(buffer_number)
+  local running = 0
+  local passed = 0
+  local failed = 0
+  local total = 0
+
+  for _, adapter_identifier in pairs(require('neotest').state.adapter_ids()) do
+    local test_case_numbers = require('neotest').state.status_counts(
+      adapter_identifier,
+      { buffer = buffer_number }
+    ) or {}
+
+    running = running + (test_case_numbers.running or 0)
+    passed = passed + (test_case_numbers.passed or 0)
+    failed = failed + (test_case_numbers.failed or 0)
+    total = total + (test_case_numbers.total or 0)
+  end
+
+  return { running = running, passed = passed, failed = failed, total = total }
+end
+
 local TestBlock = {
-  condition = function()
-    local ok, result = pcall(vim.api.nvim_call_function, 'ultest#is_test_file', {})
-    return ok and result > 0
+  condition = function(self)
+    local window_identifier = vim.fn.win_getid(self.winnr)
+    self.buffer_number = vim.api.nvim_win_get_buf(window_identifier)
+    local neotest_available, _ = pcall(require, 'neotest')
+    return neotest_available and has_test_cases(self.buffer_number)
   end,
   init = function(self)
-    local status = vim.api.nvim_call_function('ultest#status', {})
-    self.running = status.running
-    self.passed = status.passed
-    self.failed = status.failed
-    self.tests = 0
-
-    for _, entry in pairs(vim.b.ultest_tests or {}) do
-      if entry.type == 'test' then
-        self.tests = self.tests + 1
-      end
-    end
+    local test_case_numbers = get_test_case_numbers(self.buffer_number)
+    self.running = test_case_numbers.running
+    self.passed = test_case_numbers.passed
+    self.failed = test_case_numbers.failed
+    self.total = test_case_numbers.total
   end,
   surround({ '', '' }, function()
     return get_highlight('GreyBackgroundContrast').bg
@@ -30,7 +66,7 @@ local TestBlock = {
         return self.running > 0
       end,
       provider = function(self)
-        return vim.g.ultest_running_sign .. ' ' .. self.running .. ' '
+        return running_icon .. ' ' .. self.running .. ' '
       end,
       hl = get_highlight('YellowBold'),
     },
@@ -39,7 +75,7 @@ local TestBlock = {
         return self.passed > 0
       end,
       provider = function(self)
-        return vim.g.ultest_pass_sign .. ' ' .. self.passed .. ' '
+        return passed_icon .. ' ' .. self.passed .. ' '
       end,
       hl = get_highlight('GreenBold'),
     },
@@ -48,7 +84,7 @@ local TestBlock = {
         return self.failed > 0
       end,
       provider = function(self)
-        return vim.g.ultest_fail_sign .. ' ' .. self.failed .. ' '
+        return failed_icon .. ' ' .. self.failed .. ' '
       end,
       hl = get_highlight('RedBold'),
     },
@@ -60,8 +96,7 @@ local TestBlock = {
           output = ' '
         end
 
-        output = output .. self.tests .. ' Tests '
-        return output
+        return output .. self.total .. ' Tests '
       end,
       hl = get_highlight('WhiteBold'),
     },
