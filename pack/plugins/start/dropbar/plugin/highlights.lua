1d4da6f7f3c68aad2844c1ee7965f9ee30f7bf8f
Thore Strassburg <thore@weilbier.net>
2023-10-24 16:30:14 +0200

    DropBar: fix highlight loading order

---
+++ pack/plugins/start/dropbar/plugin/highlights.lua
@@ -0,0 +1,13 @@
+for name, settings in pairs(require('lsp.symbols')) do
+  vim.api.nvim_set_hl(0, 'DropBarKind' .. name, { link = settings.highlight, force = true })
+  vim.api.nvim_set_hl(0, 'DropBarIconKind' .. name, { link = 'DropBarKind' .. name, force = true })
+end
+
+vim.api.nvim_set_hl(0, 'DropBarKindFolder', { link = 'LightGreyItalic', force = true })
+vim.api.nvim_set_hl(0, 'DropBarIconKindFolder', { link = 'DropBarKindFolder', force = true })
+vim.api.nvim_set_hl(0, 'DropBarIconUIIndicator', { link = 'LightGreyBold', force = true })
+vim.api.nvim_set_hl(0, 'DropBarPreview', { link = 'GreyBackground', force = true })
+vim.api.nvim_set_hl(0, 'DropBarMenuHoverEntry', { link = 'PmenuSel', force = true })
+vim.api.nvim_set_hl(0, 'DropBarHover', { link = 'GreyBackground' })
+vim.api.nvim_set_hl(0, 'DropBarMenuCurrentContext', { link = 'DarkGreyBackground', force = true })
+vim.api.nvim_set_hl(0, 'DropBarMenuNormalFloat', { link = 'Pmenu', force = true })
