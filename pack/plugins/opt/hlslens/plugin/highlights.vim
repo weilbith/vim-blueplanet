380bca8ab15b45cab6281bc9840ab75d3fa3b9d0
Thore Strassburg <thore@weilbier.net>
2023-11-28 14:21:28 +0100

    HlsLens: remove plugin

--- pack/plugins/opt/hlslens/plugin/highlights.vim
+++
@@ -1,4 +0,0 @@
-highlight! link HlSearchNear      Search
-highlight! link HlSearchLens      IncSearch
-highlight! link HlSearchLensNear  Search
-highlight! link HlSearchFloat     IncSearch
--- pack/plugins/opt/hlslens/register_plugin.lua
+++
@@ -1,8 +0,0 @@
-require('packer').use({
-  'kevinhwang91/nvim-hlslens',
-  event = { 'CmdlineEnter /,\\?' },
-  setup = function()
-    vim.g.loaded_nvim_hlslens = true
-  end,
-  config = 'vim.cmd("packadd hlslens")',
-})
