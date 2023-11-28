c01b9fbb5304832eaee7dc656d53c29a732e1c48
Thore Strassburg <thore@weilbier.net>
2023-11-28 14:29:07 +0100

    Ufo: simplify folding text a little

--- pack/plugins/opt/ufo/plugin/setup.lua
+++ pack/plugins/opt/ufo/plugin/setup.lua
@@ -11,7 +11,7 @@ require('ufo').setup({
   end,
   fold_virt_text_handler = function(fold_text, start_line_number, end_line_number)
     local line_count = end_line_number - start_line_number
-    local suffix = '  ' .. line_count .. ' lines '
+    local suffix = '  󰇘 ' .. line_count .. ' lines'
     table.insert(fold_text, { suffix, 'Comment' })
     return fold_text
   end,
