a3f9b4c802ae85f3549f4b9a30c32f99674eedbb
Thore Strassburg <thore@weilbier.net>
2023-11-28 14:11:00 +0100

    InlayHints: align with new NeoVim API

--- pack/lsp/start/inlay_hints/plugin/autocommands.lua
+++ pack/lsp/start/inlay_hints/plugin/autocommands.lua
@@ -1,16 +1,41 @@
 local group_identifier = vim.api.nvim_create_augroup('InlayHints', {})
 
-vim.api.nvim_create_autocmd({ 'LspAttach', 'LspDetach' }, {
+local function inlay_hints_are_supported(buffer_number)
+  local clients_with_support = vim.lsp.get_clients({
+    method = 'textDocumen/inlayHint',
+    bufnr = buffer_number,
+  })
+  return #clients_with_support > 0
+end
+
+vim.api.nvim_create_autocmd({ 'LspAttach', 'InsertLeave' }, {
   group = group_identifier,
   callback = function(arguments)
-    local buffer_number = arguments.buf
-    local clients = vim.lsp.get_active_clients({ bufnr = buffer_number })
+    if inlay_hints_are_supported(arguments.buf) then
+      vim.lsp.inlay_hint.enable(arguments.buf, true)
+    end
+  end,
+})
 
-    local clients_with_support = vim.tbl_filter(function(client)
-      return client.supports_method('textDocument/inlayHint')
-    end, clients)
+vim.api.nvim_create_autocmd({ 'InsertEnter' }, {
+  group = group_identifier,
+  callback = function(arguments)
+    if inlay_hints_are_supported(arguments.buf) then
+      vim.lsp.inlay_hint.enable(arguments.buf, false)
+    end
+  end,
+})
 
-    local enable = #clients_with_support > 0
-    vim.lsp.buf.inlay_hint(buffer_number, enable)
+-- UUUUGLY workaround to redraw hints based on filtering.
+-- Inlay hints get only redrawn if the buffer version has changed since last
+-- time. This gets reset when toggling as the state is cleared.
+-- TODO: avoid flickering within same section...
+vim.api.nvim_create_autocmd({ 'CursorHold' }, {
+  group = group_identifier,
+  callback = function(arguments)
+    if vim.lsp.inlay_hint.is_enabled(arguments.buf) then
+      vim.lsp.inlay_hint.enable(arguments.buf, false)
+      vim.lsp.inlay_hint.enable(arguments.buf, true)
+    end
   end,
 })
