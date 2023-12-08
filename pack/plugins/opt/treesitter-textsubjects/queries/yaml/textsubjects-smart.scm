c9ae706c84b9affc4404b99bf40e404c3d7f81de
Thore Strassburg <thore@weilbier.net>
2023-12-08 16:22:09 +0100

    TextSubjetsSmart: add subjects for YAML

---
+++ pack/plugins/opt/treesitter-textsubjects/queries/yaml/textsubjects-smart.scm
@@ -0,0 +1,14 @@
+(
+  (block_mapping) @_start @_end
+  (#make-range! "range" @_start @_end)
+)
+
+(
+  (block_mapping_pair) @_start @_end
+  (#make-range! "range" @_start @_end)
+)
+
+(
+  (block_node) @_start @_end
+  (#make-range! "range" @_start @_end)
+)
