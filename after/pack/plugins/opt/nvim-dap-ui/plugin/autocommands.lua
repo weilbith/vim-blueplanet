b7ed1655431742fe2ff3dbca9dd6785555124169
Thore Strassburg <thore@weilbier.net>
2023-12-14 12:24:47 +0100

    nvim-dap-ui: remove status column for its windows

---
+++ after/pack/plugins/opt/nvim-dap-ui/plugin/autocommands.lua
@@ -0,0 +1,14 @@
+vim.api.nvim_create_augroup('DAP-UI', {})
+vim.api.nvim_create_autocmd('BufWinEnter', {
+  pattern = '*',
+  group = 'DAP-UI',
+  callback = function(arguments)
+    local file_type = vim.bo[arguments.buf].ft
+    local is_dap_ui_buffer = file_type:sub(1, #'dapui') == 'dapui'
+
+    if is_dap_ui_buffer then
+      -- How to access correct window by buffer number?
+      vim.opt_local.statuscolumn = ''
+    end
+  end,
+})
