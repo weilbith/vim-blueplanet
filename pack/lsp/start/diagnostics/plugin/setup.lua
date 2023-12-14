93f2ab96aa16460af22a0a6d61e6927b88468d49
Thore Strassburg <thore@weilbier.net>
2023-12-14 13:28:20 +0100

    chore: remove stupid require statement for Vim API

--- pack/lsp/start/diagnostics/plugin/setup.lua
+++ pack/lsp/start/diagnostics/plugin/setup.lua
@@ -4,7 +4,7 @@ local function number_to_circular_number(_, index)
   return (circular_numbers[index] or index) .. ' '
 end
 
-require('vim.diagnostic').config({
+vim.diagnostic.config({
   update_in_insert = true,
   severity_sort = true,
   virtual_text = {
