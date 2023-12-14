ebb55d9148c281552050915c88668cec599184ad
Thore Strassburg <thore@weilbier.net>
2023-12-14 14:15:27 +0100

    lsp-rename: add mapping to do forgotten rename

---
+++ pack/lsp/start/rename/plugin/mappings.lua
@@ -0,0 +1,10 @@
+vim.keymap.set('n', '<leader>rr', vim.lsp.buf.rename, { unique = true })
+vim.keymap.set('n', '<leader>rR', vim.lsp.buf.rename) -- Backup for fallback if overwrite failed.
+
+vim.keymap.set('n', '<leader>rf', function()
+  local new_name = vim.fn.expand('<cword>')
+  vim.cmd('undo!')
+  vim.lsp.buf.rename(new_name)
+end, {
+  desc = 'apply forgotten symbol rename for last text edit',
+})
--- plugin/lsp_mappings.vim
+++ plugin/lsp_mappings.vim
@@ -1,5 +1,4 @@
 " Most LSP related mappings are defined in the 'lsp' packages.
 
 nnoremap <unique> <leader>tr <cmd>lua vim.lsp.buf.references()<CR>
-nnoremap <unique> <leader>rr <cmd>lua vim.lsp.buf.rename()<CR>
 nnoremap <unique> <leader>ra <cmd>lua vim.lsp.buf.code_action()<CR>
