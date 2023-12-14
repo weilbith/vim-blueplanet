467da9b806426f2f0164151679d8b82380f66987
Thore Strassburg <thore@weilbier.net>
2023-12-14 13:28:56 +0100

    inlay-hints: fix toggle command after core changes
    These NeoVim core API changes already caused issues in the related
    inline hint auto-commands. This command was just forgotten because
    I haven't used it for quite some time.

--- pack/lsp/start/inlay_hints/plugin/commands.lua
+++ pack/lsp/start/inlay_hints/plugin/commands.lua
@@ -1,5 +1,7 @@
 vim.api.nvim_create_user_command('ToggleInlayHints', function()
-  vim.lsp.inlay_hint(0, nil)
+  local buffer_number = 0
+  local is_enabled = vim.lsp.inlay_hint.is_enabled(buffer_number)
+  vim.lsp.inlay_hint.enable(0, not is_enabled)
 end, {
   desc = 'Toggle the visibility of LSP inlay hints for the current buffer.',
 })
