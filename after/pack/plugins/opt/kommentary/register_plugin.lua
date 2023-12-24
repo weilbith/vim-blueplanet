829eae3d4dea4e998cd6083609133d98fe049af1
Thore Strassburg <thore@weilbier.net>
2023-12-25 00:15:10 +0100

    Kommentary: fix lazy loading of dependency module

--- after/pack/plugins/opt/kommentary/register_plugin.lua
+++ after/pack/plugins/opt/kommentary/register_plugin.lua
@@ -2,7 +2,7 @@ require('packer').use({
   'b3nj5m1n/kommentary',
   requires = {
     'JoosepAlviste/nvim-ts-context-commentstring',
-    module = 'nvim-ts-context-commentstring',
+    module = 'ts_context_commentstring',
   },
   keys = {
     '<Plug>kommentary_line_default',
