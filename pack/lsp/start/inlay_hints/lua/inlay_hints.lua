c3a31af48aae2a526c592d413400385a38aa65fe
Thore Strassburg <thore@strassburg.me>
2024-03-19 17:11:19 +0100

    Fix(inlay_hint): solve toggling issues
    After turning off inlay hints with the custom toggle command, hints
    start to appear again after some time. That was due to the
    auto-commands, which still trigger them again. As they can't determine
    the toggling state, a proper solution is to remove the auto-commands
    when turning the hints off. That required some restructuring of code.

---
+++ pack/lsp/start/inlay_hints/lua/inlay_hints.lua
@@ -0,0 +1,52 @@
+local group_identifier = vim.api.nvim_create_augroup('InlayHints', {})
+
+local function inlay_hints_are_supported(buffer_number)
+  local clients_with_support = vim.lsp.get_clients({
+    method = 'textDocumen/inlayHint',
+    bufnr = buffer_number,
+  })
+  return #clients_with_support > 0
+end
+
+local function create_auto_commands()
+  vim.api.nvim_create_autocmd({ 'LspAttach', 'InsertLeave' }, {
+    group = group_identifier,
+    callback = function(arguments)
+      if inlay_hints_are_supported(arguments.buf) then
+        vim.lsp.inlay_hint.enable(arguments.buf, true)
+      end
+    end,
+  })
+
+  vim.api.nvim_create_autocmd({ 'InsertEnter' }, {
+    group = group_identifier,
+    callback = function(arguments)
+      if inlay_hints_are_supported(arguments.buf) then
+        vim.lsp.inlay_hint.enable(arguments.buf, false)
+      end
+    end,
+  })
+
+  -- UUUUGLY workaround to redraw hints based on filtering.
+  -- Inlay hints get only redrawn if the buffer version has changed since last
+  -- time. This gets reset when toggling as the state is cleared.
+  -- TODO: avoid flickering within same section...
+  vim.api.nvim_create_autocmd({ 'CursorHold' }, {
+    group = group_identifier,
+    callback = function(arguments)
+      if vim.lsp.inlay_hint.is_enabled(arguments.buf) then
+        vim.lsp.inlay_hint.enable(arguments.buf, false)
+        vim.lsp.inlay_hint.enable(arguments.buf, true)
+      end
+    end,
+  })
+end
+
+local function clear_auto_commands()
+  vim.api.nvim_clear_autocmds({ group = group_identifier })
+end
+
+return {
+  create_auto_commands = create_auto_commands,
+  clear_auto_commands = clear_auto_commands,
+}
--- pack/lsp/start/inlay_hints/plugin/commands.lua
+++ pack/lsp/start/inlay_hints/plugin/commands.lua
@@ -1,7 +1,17 @@
+local create_auto_commands = require('inlay_hints').create_auto_commands
+local clear_auto_commands = require('inlay_hints').clear_auto_commands
+
 vim.api.nvim_create_user_command('ToggleInlayHints', function()
   local buffer_number = 0
   local is_enabled = vim.lsp.inlay_hint.is_enabled(buffer_number)
-  vim.lsp.inlay_hint.enable(0, not is_enabled)
+
+  if is_enabled then
+    clear_auto_commands()
+    vim.lsp.inlay_hint.enable(0, false)
+  else
+    create_auto_commands()
+    vim.lsp.inlay_hint.enable(0, true)
+  end
 end, {
   desc = 'Toggle the visibility of LSP inlay hints for the current buffer.',
 })
