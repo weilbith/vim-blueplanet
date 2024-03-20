2963be486ab32e743b112256e740914fa1a99257
Thore Strassburg <thore@strassburg.me>
2024-03-20 17:39:18 +0100

    Feat(lsp-file-operations): replace custom implementation with plugin

--- after/pack/plugins/opt/neo-tree.nvim/plugin/setup.lua
+++ after/pack/plugins/opt/neo-tree.nvim/plugin/setup.lua
@@ -88,22 +88,6 @@ require('neo-tree').setup({
     group_empty_dirs = true,
     scan_mode = 'deep',
   },
-  -- Check if something is a folder or a file.
-  -- If server does not handle folders, get all files in folder.
-  event_handlers = {
-    {
-      event = 'file_renamed',
-      handler = function(arguments)
-        require('custom.lsp.file_operations').rename(arguments.source, arguments.destination)
-      end,
-    },
-    {
-      event = 'file_moved',
-      handler = function(arguments)
-        require('custom.lsp.file_operations').rename(arguments.source, arguments.destination)
-      end,
-    },
-  },
   source_selector = {
     winbar = true,
     content_layout = 'center',
@@ -130,3 +114,5 @@ require('neo-tree').setup({
     },
   },
 })
+
+require('lsp-file-operations').setup()
--- after/pack/plugins/opt/neo-tree.nvim/register_plugin.lua
+++ after/pack/plugins/opt/neo-tree.nvim/register_plugin.lua
@@ -1,10 +1,10 @@
 require('packer').use({
   'nvim-neo-tree/neo-tree.nvim',
-  branch = 'v2.x',
   requires = {
     'nvim-lua/plenary.nvim',
     { 'kyazdani42/nvim-web-devicons', module = 'nvim-web-devicons' },
     { 'MunifTanjim/nui.nvim', module = 'nui' },
+    { 'antosha417/nvim-lsp-file-operations', module = 'lsp-file-operations' },
   },
   cmd = 'Neotree',
 })
