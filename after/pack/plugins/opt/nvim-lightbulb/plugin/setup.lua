db528f6a855081c4f3db13a7808246a6b8b4a53b
Thore Strassburg <thore@strassburg.me>
2024-11-13 17:47:06 +0100

    feat(nvim-lightbulb): use new feature for code lenses

--- after/pack/plugins/opt/nvim-lightbulb/plugin/setup.lua
+++ after/pack/plugins/opt/nvim-lightbulb/plugin/setup.lua
@@ -1,10 +1,13 @@
 require('nvim-lightbulb').setup({
+  code_lenses = true,
+  action_kinds = { "quickfix" },
   sign = {
     enabled = false,
   },
   float = {
     enabled = true,
-    text = '',
+    text = '',
+    lens_text = '󱡴',
     win_opts = {
       winblend = 60,
     },
