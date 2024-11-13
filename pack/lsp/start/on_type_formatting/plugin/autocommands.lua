665706936c78bf359077cb52db4cb7337aef0345
Thore Strassburg <thore@strassburg.me>
2024-11-13 17:54:05 +0100

    fix(lsp/on_type_formatting): fix autocmd group

--- pack/lsp/start/on_type_formatting/plugin/autocommands.lua
+++ pack/lsp/start/on_type_formatting/plugin/autocommands.lua
@@ -1,12 +1,10 @@
 local configuration = require('on_type_formatting.configuration')
 local key_listening = require('on_type_formatting.key_listening')
 
-local auto_command_group_name = 'Lsp/OnTypeFormatting'
-
-vim.api.nvim_create_augroup(auto_command_group_name, {})
+local group = vim.api.nvim_create_augroup('Lsp/OnTypeFormatting', {})
 
 vim.api.nvim_create_autocmd('LspAttach', {
-  group = auto_command_group_name,
+  group = group,
   callback = function(event)
     local client_identifier = (event.data or {}).client_id
     configuration.maybe_add_configuration_for_client(client_identifier, event.buf)
@@ -15,7 +13,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
 })
 
 vim.api.nvim_create_autocmd('LspDetach', {
-  group = auto_command_group_name,
+  group = group,
   callback = function(event)
     local client_identifier = (event.data or {}).client_id
     configuration.maybe_remove_configuration_for_client(client_identifier, event.buf)
