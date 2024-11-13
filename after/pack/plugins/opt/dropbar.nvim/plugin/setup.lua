b5747fd95bebc3ae5fd726b5cd41938fc1f79732
Thore Strassburg <thore@strassburg.me>
2024-11-13 18:04:45 +0100

    chore(dropbar.nvim): update to breaking config changes

--- after/pack/plugins/opt/dropbar.nvim/plugin/setup.lua
+++ after/pack/plugins/opt/dropbar.nvim/plugin/setup.lua
@@ -1,10 +1,6 @@
 local constants = require('dropbar_custom.constants')
 
 require('dropbar').setup({
-  general = {
-    -- Rather embed this manually into separated winbar plugin.
-    enable = false,
-  },
   icons = {
     symbols = constants.symbols,
     ui = {
@@ -17,6 +13,7 @@ require('dropbar').setup({
     },
   },
   bar = {
+    enable = false, -- Rather embed this manually into separated winbar plugin.
     padding = {
       left = 0,
       right = 0,
