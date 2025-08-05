1602f4c9f2d7d4f3dddba68780991dff73a8fe23
Thore Strassburg <thore@strassburg.me>
2025-08-05 15:20:41 +0200

    feat(nvim-cmp): apply text-edits for automatic confirmations

--- after/pack/plugins/opt/nvim-cmp/lua/custom/cmp/automatic_confirmation.lua
+++ after/pack/plugins/opt/nvim-cmp/lua/custom/cmp/automatic_confirmation.lua
@@ -14,8 +14,13 @@ end
 
 local function maybe_confirm_last_selected_completion_entry()
   if last_selected_completion_entry ~= nil then
-    cmp.core:confirm(last_selected_completion_entry, {})
+    local completion_item = last_selected_completion_entry:get_completion_item()
+    local addition_text_edits = completion_item.additionalTextEdits or {}
+    local offset_encoding = last_selected_completion_entry.source:get_position_encoding_kind()
+    vim.lsp.util.apply_text_edits(addition_text_edits, 0, offset_encoding)
     clear_last_selected_completion_entry()
+
+    -- TODO: remove at some point
   end
 end
 
