965e6cd74cea4090c7a2bc5d461670f02ca7e814
Thore Strassburg <thore@strassburg.me>
2025-08-05 15:11:48 +0200

    docs(gitsigns.nvim): add descriptions for key mappings
    This also alternates the mapping to show inline diff for a hunk. Just
    because it didn't seam to be that intuitive anymore.

--- after/pack/plugins/opt/gitsigns.nvim/plugin/mappings.lua
+++ after/pack/plugins/opt/gitsigns.nvim/plugin/mappings.lua
@@ -13,18 +13,62 @@ local function compose_functions(g, f)
   end
 end
 
-vim.keymap.set('n', '<leader>gm', git_signs.blame_line, {})
-vim.keymap.set('n', '<leader>ghh', git_signs.preview_hunk_inline, {})
+vim.keymap.set(
+  'n',
+  '<leader>gm',
+  git_signs.blame_line,
+  { desc = 'show/blame git commit for line under cursor' }
+)
 
-vim.keymap.set('n', '<leader>ghs', git_signs.stage_hunk, {})
-vim.keymap.set('v', '<leader>gs', compose_functions(git_signs.stage_hunk, get_visual_range), {})
-vim.keymap.set('n', '<leader>gS', git_signs.stage_buffer, {})
+vim.keymap.set(
+  'n',
+  '<leader>ghd',
+  git_signs.preview_hunk_inline,
+  { desc = 'preview the diff of the git hunk under the cursor' }
+)
 
-vim.keymap.set('n', '<leader>ghr', git_signs.reset_hunk, {})
-vim.keymap.set('v', '<leader>gr', compose_functions(git_signs.reset_hunk, get_visual_range), {})
-vim.keymap.set('n', '<leader>gR', git_signs.reset_buffer, {})
+vim.keymap.set(
+  'n',
+  '<leader>ghs',
+  git_signs.stage_hunk,
+  { desc = 'stage the current git change hunk under the cursor' }
+)
 
-vim.keymap.set({ 'v', 'o', 'x' }, 'ih', git_signs.select_hunk, {})
+vim.keymap.set(
+  'v',
+  '<leader>gs',
+  compose_functions(git_signs.stage_hunk, get_visual_range),
+  { desc = 'stage the selected git changes' }
+)
+
+vim.keymap.set(
+  'n',
+  '<leader>gS',
+  git_signs.stage_buffer,
+  { desc = 'stage the whole buffer with all git changes' }
+)
+
+vim.keymap.set('n', '<leader>ghr', git_signs.reset_hunk, {
+  desc = 'reset the git change hunk under the cursor',
+})
+
+vim.keymap.set('v', '<leader>gr', compose_functions(git_signs.reset_hunk, get_visual_range), {
+  desc = 'reset the selected git changes',
+})
+
+vim.keymap.set(
+  'n',
+  '<leader>gR',
+  git_signs.reset_buffer,
+  { desc = 'reset the whole buffer with all git changes' }
+)
+
+vim.keymap.set(
+  { 'v', 'o', 'x' },
+  'ih',
+  git_signs.select_hunk,
+  { desc = 'select range of surrounding git change hunk' }
+)
 
 vim.keymap.set('n', ']c', function()
   if vim.wo.diff then
@@ -32,7 +76,9 @@ vim.keymap.set('n', ']c', function()
   else
     git_signs.nav_hunk('next')
   end
-end, {})
+end, {
+  desc = 'jump to next git change hunk',
+})
 
 vim.keymap.set('n', '[c', function()
   if vim.wo.diff then
@@ -40,4 +86,6 @@ vim.keymap.set('n', '[c', function()
   else
     git_signs.nav_hunk('prev')
   end
-end, {})
+end, {
+  desc = 'jump to previous git change hunk',
+})
