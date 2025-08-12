51ffe4e7b4380567982f680b0f005db4a7586bde
Thore Strassburg <thore@strassburg.me>
2025-08-12 09:59:50 +0200

    fix(abstractions/snippets): fix jump instructions
    API seem to have changed and is inconsistent between `vim.snippet.*`
    functions.

--- pack/abstractions/start/snippets/lua/snippets/navigation.lua
+++ pack/abstractions/start/snippets/lua/snippets/navigation.lua
@@ -3,16 +3,16 @@ local DIRECTION_BACKWARDS = -1
 
 local function jump_forward()
   if vim.snippet.active({ direction = DIRECTION_FORWARDS }) then
-    vim.snippet.jump({ direction = DIRECTION_FORWARDS })
-  elseif type(vim.g.snippet_navigation_jump_forward) then
+    vim.snippet.jump(DIRECTION_FORWARDS)
+  elseif type(vim.g.snippet_navigation_jump_forward) == 'function' then
     vim.g.snippet_navigation_jump_forward()
   end
 end
 
 local function jump_backward()
   if vim.snippet.active({ direction = DIRECTION_BACKWARDS }) then
-    vim.snippet.jump({ direction = DIRECTION_BACKWARDS })
-  elseif type(vim.g.snippet_navigation_jump_backward) then
+    vim.snippet.jump(DIRECTION_BACKWARDS)
+  elseif type(vim.g.snippet_navigation_jump_backward) == 'function' then
     vim.g.snippet_navigation_jump_backward()
   end
 end
