28b79babfd847eeac80d44a13285842c3f80c24b
Thore Strassburg <thore@strassburg.me>
2025-08-12 09:58:37 +0200

    feat(gitsigns.nvim): disable blame for current line
    Trying to reduce noise in view. Can still be shown on demand.

--- after/pack/plugins/opt/gitsigns.nvim/plugin/setup.lua
+++ after/pack/plugins/opt/gitsigns.nvim/plugin/setup.lua
@@ -1,6 +1,5 @@
 require('gitsigns').setup({
   numhl = true,
   signcolumn = false, -- Doing it manually in the custom status column.
-  current_line_blame = true,
   attach_to_untracked = true,
 })
