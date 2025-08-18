cca9d709efd982f0b09e1cce687377c84b98b92c
Thore Strassburg <thore@strassburg.me>
2025-08-18 15:28:14 +0200

    chore(abstractions/plugin_manager): move optional Lua module
    There is no meaning in this (anymore? ever?).

--- pack/abstractions/start/plugin_manager/plugin/commands.lua
+++ pack/abstractions/start/plugin_manager/plugin/commands.lua
@@ -1,9 +1,9 @@
 vim.api.nvim_create_user_command('UpdatePlugins', function()
   vim.cmd.packadd('plugin_manager')
-  require('plugin_manager').update()
+  require('plugin_manager.installation').update()
 end, { desc = 'Update all plugins and install missing ones' })
 
 vim.api.nvim_create_user_command('InstallNewPlugins', function()
   vim.cmd.packadd('plugin_manager')
-  require('plugin_manager').install_new()
+  require('plugin_manager.installation').install_new()
 end, { desc = 'Install only all missing/new plugins' })
