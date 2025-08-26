3d446012da026c8c5412402fb750b39fd499fb8e
Thore Strassburg <thore@strassburg.me>
2025-08-26 08:48:46 +0200

    feat(plugins/otter.nvim): add as new plugin
    The original intention was to also use it for LSP hint/signature/...
    pop-up windows. But that does not work as expected. For good reason.
    Anyhow, the plugin is still fancy and useful. Using it for Markdown
    fences only atm.

---
+++ after/pack/plugins/start/otter.nvim/plugin/autocommands.lua
@@ -0,0 +1,1 @@
+require('plugin_manager').lazy_load_plugin_on_lua_module('otter.nvim', 'otter')
---
+++ after/pack/plugins/start/otter.nvim/plugin_specification.lua
@@ -0,0 +1,4 @@
+require('packer').use({ 'jmbuhr/otter.nvim', opt = true })
+
+-- Dependencies
+require('packer').use({ 'nvim-treesitter/nvim-treesitter', opt = true })
