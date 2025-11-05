9a47f1653a26d0f58278ef19e79657a9d9d90992
Thore Strassburg <thore@strassburg.me>
2025-11-05 09:01:24 +0100

    feat(ftdetect): add rust-script detection
    The rust-script project recommends to name rust scripts with the `ers`
    file extension for better Windows support.
    Therefore, it also adapts the configuration for the rust-analyzer as
    there is no Cargo configuration "visible" for such scripts.

--- lsp/rust-analyzer.lua
+++ lsp/rust-analyzer.lua
@@ -2,7 +2,7 @@
 return {
   cmd = { 'rust-analyzer' },
   filetypes = { 'rust' },
-  root_markers = { 'Cargo.toml' },
+  root_markers = { 'Cargo.toml', '.git' },
   capabilities = {
     experimental = {
       serverStatusNotification = true,
