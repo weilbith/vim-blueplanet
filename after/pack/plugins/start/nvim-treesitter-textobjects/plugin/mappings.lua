f493a5ea8075a456bbb69ae6a61bc68fbaa575e4
Thore Strassburg <thore@strassburg.me>
2025-11-12 11:25:37 +0100

    fix(plugins/nvim-treesitter-textobjects): update key mappings
    Adapt to the latest updates of the plugin without commands and raw Lua
    functions only.

--- after/pack/plugins/start/nvim-treesitter-textobjects/plugin/mappings.lua
+++ after/pack/plugins/start/nvim-treesitter-textobjects/plugin/mappings.lua
@@ -1,101 +1,100 @@
-require('plugin_manager').lazy_load_plugin_on_command('nvim-treesitter-textobjects', {
-  'TSTextobjectSelect',
-  'TSTextobjectGotoNextStart',
-  'TSTextobjectGotoPreviousStart',
-  'TSTextobjectGotoNextEnd',
-  'TSTextobjectGotoPreviousEnd',
-})
+require('plugin_manager').lazy_load_plugin_on_lua_module(
+  'nvim-treesitter-textobjects',
+  'nvim-treesitter-textobjects'
+)
+
+local function select()
+  return require('nvim-treesitter-textobjects.select')
+end
+
+local function move()
+  return require('nvim-treesitter-textobjects.move')
+end
 
 vim.keymap.set({ 'o', 'v' }, 'am', function()
-  vim.cmd.TSTextobjectSelect('@function.outer')
+  select().select_textobject('@function.outer', 'textobjects')
 end, { desc = 'outer function text object' })
 
 vim.keymap.set({ 'o', 'v' }, 'im', function()
-  vim.cmd.TSTextobjectSelect('@function.inner')
+  select().select_textobject('@function.inner')
 end, { desc = 'inner function text object' })
 
 vim.keymap.set({ 'n', 'o', 'v' }, ']m', function()
-  vim.cmd.TSTextobjectGotoNextStart('@function.outer')
+  move().goto_next_start('@function.outer')
 end, { desc = 'jump to start of next function' })
 
 vim.keymap.set({ 'n', 'o', 'v' }, '[m', function()
-  vim.cmd.TSTextobjectGotoPreviousStart('@function.outer')
+  move().goto_previous_start('@function.outer')
 end, { desc = 'jump to start of previous/current function' })
 
 vim.keymap.set({ 'n', 'o', 'v' }, ']M', function()
-  vim.cmd.TSTextobjectGotoNextEnd('@function.outer')
+  move().goto_next_end('@function.outer')
 end, { desc = 'jump to end of next/current function' })
 
 vim.keymap.set({ 'n', 'o', 'v' }, '[M', function()
-  vim.cmd.TSTextobjectGotoPreviousEnd('@function.outer')
+  move().goto_previous_end('@function.outer')
 end, { desc = 'jump to end of previous function' })
 
 vim.keymap.set({ 'o', 'v' }, 'ac', function()
-  vim.cmd.TSTextobjectSelect('@class.outer')
+  select().select_textobject('@class.outer')
 end, { desc = 'outer class text object' })
 
 vim.keymap.set({ 'o', 'v' }, 'ic', function()
-  vim.cmd.TSTextobjectSelect('@class.inner')
+  select().select_textobject('@class.inner')
 end, { desc = 'inner class text object' })
 
 vim.keymap.set({ 'n', 'o', 'v' }, ']c', function()
-  vim.cmd.TSTextobjectGotoNextStart('@class.outer')
+  move().goto_next_start('@class.outer')
 end, { desc = 'jump to start of next class' })
 
 vim.keymap.set({ 'n', 'o', 'v' }, '[c', function()
-  vim.cmd.TSTextobjectGotoPreviousStart('@class.outer')
+  move().goto_previous_start('@class.outer')
 end, { desc = 'jump to start of previous/current class' })
 
 vim.keymap.set({ 'n', 'o', 'v' }, ']C', function()
-  vim.cmd.TSTextobjectGotoNextEnd('@class.outer')
+  move().goto_next_end('@class.outer')
 end, { desc = 'jump to end of next/current class' })
 
 vim.keymap.set({ 'n', 'o', 'v' }, '[C', function()
-  vim.cmd.TSTextobjectGotoPreviousEnd('@class.outer')
+  move().goto_previous_end('@class.outer')
 end, { desc = 'jump to end previous class' })
 
 vim.keymap.set({ 'o', 'v' }, 'aa', function()
-  vim.cmd.TSTextobjectSelect('@parameter.outer')
+  select().select_textobject('@parameter.outer')
 end, { desc = 'parameter class text object' })
 
 vim.keymap.set({ 'o', 'v' }, 'ia', function()
-  vim.cmd.TSTextobjectSelect('@parameter.inner')
+  select().select_textobject('@parameter.inner')
 end, { desc = 'inner parameter text object' })
 
 vim.keymap.set({ 'n', 'o', 'v' }, ']a', function()
-  vim.cmd.TSTextobjectGotoNextStart('@parameter.outer')
+  move().goto_next_start('@parameter.outer')
 end, { desc = 'jump to start of next parameter' })
 
 vim.keymap.set({ 'n', 'o', 'v' }, ']A', function()
-  vim.cmd.TSTextobjectGotoNextEnd('@parameter.outer')
+  move().goto_next_end('@parameter.outer')
 end, { desc = 'jump to start of previous/current parameter' })
 
 vim.keymap.set({ 'n', 'o', 'v' }, '[a', function()
-  vim.cmd.TSTextobjectGotoPreviousStart('@parameter.outer')
+  move().goto_previous_start('@parameter.outer')
 end, { desc = 'jump to end of next/current parameter' })
 
 vim.keymap.set({ 'n', 'o', 'v' }, '[A', function()
-  vim.cmd.TSTextobjectGotoPreviousEnd('@parameter.outer')
+  move().goto_previous_end('@parameter.outer')
 end, { desc = 'jump to end previous parameter' })
 
 vim.keymap.set({ 'o', 'v' }, 'a/', function()
-  vim.cmd.TSTextobjectSelect('@comment.outer')
+  select().select_textobject('@comment.outer')
 end, { desc = 'outer comment text object' })
 
 vim.keymap.set({ 'o', 'v' }, 'i/', function()
-  vim.cmd.TSTextobjectSelect('@comment.outer')
+  select().select_textobject('@comment.outer')
 end, { desc = 'inner comment text object' })
 
-vim.keymap.set(
-  { 'n', 'o', 'v' },
-  ']/ :',
-  'c-u>TSTextobjectGotoNextStart @comment.outer<cr>',
-  { desc = 'jump to start of next comment' }
-)
+vim.keymap.set({ 'n', 'o', 'v' }, ']/', function()
+  move().goto_next_start('@comment.outer')
+end, { desc = 'jump to start of next comment' })
 
-vim.keymap.set(
-  { 'n', 'o', 'v' },
-  '[/ :',
-  'c-u>TSTextobjectGotoPreviousStart @comment.outer<cr>',
-  { desc = 'jump to start of previous/current comment' }
-)
+vim.keymap.set({ 'n', 'o', 'v' }, '[/', function()
+  move().goto_previous_start('@comment.outer')
+end, { desc = 'jump to start of previous/current comment' })
