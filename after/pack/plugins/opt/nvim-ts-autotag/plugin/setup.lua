a9b32f84e215a3ac1ca74c45eb8d6e09460409c7
Thore Strassburg <thore@strassburg.me>
2025-11-13 00:04:35 +0100

    feat(plugins/nvim-ts-autotag): disable rename in favor of LSP

--- after/pack/plugins/opt/nvim-ts-autotag/plugin/setup.lua
+++ after/pack/plugins/opt/nvim-ts-autotag/plugin/setup.lua
@@ -1,3 +1,8 @@
 vim.cmd.packadd('nvim-treesitter')
 
-require('nvim-ts-autotag').setup()
+require('nvim-ts-autotag').setup({
+  opts = {
+    enable_close = true,
+    enable_rename = false, -- LSP linked editing ranges handles this.
+  },
+})
