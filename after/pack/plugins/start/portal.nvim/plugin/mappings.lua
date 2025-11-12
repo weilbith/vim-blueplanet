e19c6e9a14540b139c3ccef320bd06a1c6ae93e6
Thore Strassburg <thore@strassburg.me>
2025-11-12 11:41:55 +0100

    fix(plugins/portal.nvim): key mapping arguments
    For some reason, the Portal command fails to parse the arguments when
    given as plain string. Providing an argument list works fine. I could
    not reproduce this issue for other plugins.

--- after/pack/plugins/start/portal.nvim/plugin/mappings.lua
+++ after/pack/plugins/start/portal.nvim/plugin/mappings.lua
@@ -1,9 +1,9 @@
 require('plugin_manager').lazy_load_plugin_on_command('portal.nvim', 'Portal')
 
 vim.keymap.set('n', '<C-o>', function()
-  vim.cmd.Portal('jumplist backward')
+  vim.cmd.Portal({ args = { 'jumplist', 'backward' } })
 end, { desc = 'navigate in the jump list backwards with previews of the locations' })
 
 vim.keymap.set('n', '<C-i>', function()
-  vim.cmd.Portal('jumplist forward')
+  vim.cmd.Portal({ args = { 'jumplist', 'forward' } })
 end, { desc = 'navigate in the jump list backwards with previews of the locations' })
