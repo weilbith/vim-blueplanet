33226a30181ee3164962f8ce200177e44c4d5506
Thore Strassburg <thore@strassburg.me>
2025-11-18 20:55:08 +0100

    feat(plugins/iswap.nvim): change key mapping
    Avoid using the leader key here. It was basically "blocking" the usage
    of the 'c' key mapping group from a semantic perspective. As the 'c'
    stand for "change", I decided to align it with native change operator
    mappings. The mapping for 'cs' is used by the vim-surround plugin. It is
    a tight fit, but alright.

--- after/pack/plugins/start/iswap.nvim/plugin/mappings.lua
+++ after/pack/plugins/start/iswap.nvim/plugin/mappings.lua
@@ -2,7 +2,7 @@ require('plugin_manager').lazy_load_plugin_on_command('iswap.nvim', 'ISwapWith')
 
 vim.keymap.set(
   'n',
-  '<leader>cs',
+  'cS',
   vim.cmd.ISwapWith,
   { desc = 'swap target under cursor with sibling elements (e.g. entries of array)' }
 )
