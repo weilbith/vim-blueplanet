801666d12b667cab4dadb25da6e130aa4360e090
Thore Strassburg <thore@strassburg.me>
2025-11-18 20:53:12 +0100

    chore(plugins/symbol-usage.nvim): inline Lua module into setup script

--- after/pack/plugins/opt/symbol-usage.nvim/plugin/setup.lua
+++ after/pack/plugins/opt/symbol-usage.nvim/plugin/setup.lua
@@ -1,5 +1,5 @@
 local SymbolKind = vim.lsp.protocol.SymbolKind
-local format_lens = require('symbol-usage.custom').format_lens
+local format_lens
 
 require('symbol-usage').setup({
   hl = { link = 'LspCodeLens' },
@@ -31,3 +31,45 @@ require('symbol-usage').setup({
     },
   },
 })
+
+--- @class Record
+--- @field references? number
+--- @field implementation? number
+--- @field definition? number | nil
+
+local function format_tag(count, designator)
+  return {
+    { '', 'DarkGrey' },
+    { count .. ' ' .. designator, 'LspCodeLens' },
+    { ' ', 'DarkGrey' },
+  }
+end
+
+--- @param record Record
+--- @return table | nil
+format_lens = function(record)
+  local segments = {}
+
+  if record.references ~= nil then
+    local count = record.references
+    local designator = 'reference' .. (count == 1 and '' or 's')
+    local tag_segments = format_tag(count, designator)
+    vim.list_extend(segments, tag_segments)
+  end
+
+  if record.implementation ~= nil then
+    local count = record.implementation
+    local designator = 'implementation' .. (count == 1 and '' or 's')
+    local tag_segments = format_tag(count, designator)
+    vim.list_extend(segments, tag_segments)
+  end
+
+  if record.definition ~= nil then
+    local count = record.definition
+    local designator = 'definition' .. (count == 1 and '' or 's')
+    local tag_segments = format_tag(count, designator)
+    vim.list_extend(segments, tag_segments)
+  end
+
+  return segments
+end
